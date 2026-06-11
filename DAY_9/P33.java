cdf58bbf63f9fc1aa38051dedaf8e4d78b72dfd3
Shivansh Yadav <shivayadavmy8570238@gmail.com>
2026-06-11 15:18:46 +0530

    ADDED DAY 9

---
+++ DAY_9/P33.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class P33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
